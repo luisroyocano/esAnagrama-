3f8559ea52881f175399fa2bae71ef17b390caee
xp <xp@CETYSDAM013>
2019-01-18 12:50:12 +0100

    Versión 2 del programa que calcula los máximos. El bucle FOR ahora empieza en 1 en vez de en 0 (y funciona)

--- src/ejemplosconsola/EjemplosConsola.java
+++ src/ejemplosconsola/EjemplosConsola.java
@@ -14,32 +14,53 @@ import java.util.Arrays;
 public class EjemplosConsola {
     
     //declaro un array de ints de instancia
-    int[] listaNumeros = {20, 31, 27, 2, 5, 99};
-    
+    int[] listaNumeros = {200, 31, 27, 2, 5, 19};
+    int[] listaNumeros2 ={ 150 , 150, -11, 8,88,  -5, 15, };
+    int[] listaNumeros3 = {36, -31, 56, 2, 5, 99, 5000, 0};
+   
     /*
     el método máximos va a calcular el máximo y el segundo máximo de una lista de números
     */
     
     private int[] maximos (int[] lista){
         //declaramos un array de dos elementos para guardar el máximo y el segundo máximo
-        int[] listaMaximos = {lista[0], lista[0]};
-        for (int i=0; i< lista.length; i++){
-            if(listaMaximos[0] < lista[i]){
-                listaMaximos[1] = listaMaximos[0];
-                listaMaximos[0] = lista[i];
+        int[] listaMaximos = {lista[0],lista[1] };
+
+        
+        for (int i=0; i< lista.length ; i++){
+            if( lista[i] >= listaMaximos[0]){
+                if(i!=0){//desplazo sólo a la derecha si no estoy nusto en la primera posición
+                    listaMaximos[1] = listaMaximos[0];
+                }
+                //si llega aquí, es que el número que estoy comparando
+                //es mayor o igual que el que tengo primero en la 
+                //lista de máximos
+                //desplazo a la derecha el que había como máximo
+                listaMaximos[0] = lista[i]; //pongo en la primera posición al nuevo máximo
             }
-           
-        }
+            else if( lista[i] >= listaMaximos[1]){//compruebo si el número que esoty leyendo es mayor que el SEGUNDO  de la lista de máximos
+                listaMaximos[1] = lista[i];
+                
+            }
+                
+            }
         
         return listaMaximos;
     }
+    
+    
+   
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         EjemplosConsola ejercicios = new EjemplosConsola();
-        System.out.println(ejercicios.listaNumeros[2]);
+        
         System.out.println(Arrays.toString(ejercicios.maximos(ejercicios.listaNumeros)) );
+        
+        System.out.println(Arrays.toString(ejercicios.maximos(ejercicios.listaNumeros2)) );
+         
+        System.out.println(Arrays.toString(ejercicios.maximos(ejercicios.listaNumeros3)) );
     }
     
 }
